70aa02e69c5130b9f374818c86b75e086cadd88f
wzaghal <wisamzaghal@gmail.com>
2015-05-05 21:58:53 -0400

    add bower

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+var bower_dir = __dirname + '/bower_components';
+
 module.exports = {
   entry: ['webpack/hot/dev-server','./js/main.js','./css/main.css'],
+  resolve: {
+    alias: {
+      'react': bower_dir + '/react/react.min.js'
+    }
+  },
   output: {
     path: './build',
     filename: 'bundle.js'
