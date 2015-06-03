e4ad48e39f1a2c5143ee92003fcd1479427aba06
wzaghal <wisamzaghal@gmail.com>
2015-06-02 23:52:27 -0400

    isolate scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var bower_dir = __dirname + '/bower_components';
 
 module.exports = {
-  entry: ['webpack/hot/dev-server','./js/app.js','./scss/app.scss'],
+  entry: ['webpack/hot/dev-server','./js/app.js','./scss/app.scss', './scss/components/_Terminal.scss', './scss/components/_Title.scss'],
   resolve: {
     alias: {
       'react': bower_dir + '/react/react.min.js'
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'jsx-loader' },
-      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
+      { test: /\.scss$/, loader: 'style!css!sass?outputStyle=expanded' }
 
     ]
   }
