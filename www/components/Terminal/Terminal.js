c5f596843180729c002661f97c17604638cb35cc
wzaghal <wisamzaghal@gmail.com>
2015-06-07 22:55:51 -0400

    Add animation and break into subcomponents

--- www/components/Terminal/Terminal.js
+++ www/components/Terminal/Terminal.js
@@ -11,29 +11,72 @@ module.exports = React.createClass({
 
 });
 
+const Blink = React.createClass({
+  render: function() {
+    return (
+      <span className="cursor">|</span>
+    )
+  }
+});
+
+const TerminalOutput = React.createClass({
+  render: function(){
+    var blink;
+    var className = "terminal-interface__text terminal-interface__text--" + this.props.action;
+    if (this.props.blink){
+      blink = <Blink></Blink>
+    }
+    return (
+            <p className={className}>
+              {this.props.data}
+                {blink}
+            </p>
+          )
+  }
+});
+
+const TerminalDefault = React.createClass({
+  render: function(){
+    var blink;
+    if (this.props.blink){
+      blink = <Blink></Blink>
+    }
+    var className = "terminal-interface__root terminal-interface__root--" + this.props.action;
+    return (
+      <p className={className}>~/project git:(master) {blink} </p>
+      )
+  }
+});
+
 const TerminalScreen = React.createClass({
+
   getInitialState: function(){
-    return {'data':'theia'};
+    return {
+      prompt:'theia',
+      output:'Found 3 missing packages',
+      missingPackages: ['gulp','gulp-sass','babel']
+    };
   },
   render: function(){
-    const fullMsg = this.state.data;
-    const lastChar = this.state.data.slice(-1);
-    const terminalMsg = fullMsg.slice(0, fullMsg.length -1 );
     return (
       <div className="terminal-screen">
         <div className="terminal-interface">
-          <p className="terminal-interface__root">~/project git:(master)</p>
-          <p className="terminal-interface__text">
-            {terminalMsg}
-            <span className="terminal-interface--last-char">{lastChar}</span>
-          </p>
-          <p className="terminal-interface__text">
-            {terminalMsg}
-            <span className="terminal-interface--last-char">{lastChar}</span>
-            <span className="cursor">|</span>
-          </p>
+          <TerminalDefault></TerminalDefault>
+          <TerminalOutput data={this.state.prompt} action="type"></TerminalOutput>
+          <TerminalOutput data={this.state.output} action="delay"></TerminalOutput>
+
+          <div className="terminal-interface__text__packages">
+            {this.displayMissingPackages()}
+          </div>
+          <TerminalDefault action="delay" blink="true"></TerminalDefault>
         </div>
       </div>
       )
+  },
+  displayMissingPackages: function(){
+    return this.state.missingPackages.map(function(pckg){
+      return <TerminalOutput data={"* " + pckg} action="delay"></TerminalOutput>
+
+    });
   }
 })
