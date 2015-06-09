d8a25786fc720c7f68fc86f6bafc0d5f12751ab1
wzaghal <wisamzaghal@gmail.com>
2015-06-08 22:52:59 -0400

    add eslint to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var bower_dir = __dirname + '/bower_components';
+var exclude=/node_modules/;
 
 module.exports = {
   entry: ['webpack/hot/dev-server','./www/app.js', './www/scss/app.scss', './www/components/Terminal/_Terminal.scss' , './www/components/Title/_Title.scss'],
   resolve: {
     alias: {
-      'react': bower_dir + '/react/react.min.js'
+      'react': bower_dir + '/react/react.js'
     }
   },
   output: {
@@ -12,9 +13,17 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
+    preLoaders: [
+            {test: /(\.js$|\.jsx$)/ , exclude: [/node_modules/,bower_dir] ,loader: "eslint-loader"}
+    ],
+
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader'},
-      { test: /\.scss$/, loader: 'style!css!sass' }
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.js$/, loader: 'babel-loader', exclude: exclude},
+
     ]
+  },
+  eslint: {
+    configFile: '.eslintrc'
   }
 };
